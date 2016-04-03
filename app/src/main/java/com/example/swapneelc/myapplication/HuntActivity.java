6d6d90e60b125801b37d0d3903153de7494f9c2a
flam316 <adam@rubiverse.net>
2016-04-03 03:02:58 -0400

    TEH FINAL COUNTDOWNNNN

--- app/src/main/java/com/example/swapneelc/myapplication/HuntActivity.java
+++ app/src/main/java/com/example/swapneelc/myapplication/HuntActivity.java
@@ -2,12 +2,17 @@ package com.example.swapneelc.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HuntActivity extends AppCompatActivity {
 
@@ -20,9 +25,37 @@ public class HuntActivity extends AppCompatActivity {
 
         String teamname = i.getStringExtra("teamname");
 
+
+
         TextView teamname_view = (TextView) findViewById(R.id.teamname);
-        teamname_view.setText(teamname);
+        teamname_view.setText("Team " + teamname);
+
+
+        final TextView timeleft_view = (TextView) findViewById(R.id.timeleft);
+
+        new CountDownTimer(30*1000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                timeleft_view.setText(millisUntilFinished/1000 + "s left");
+            }
+            public void onFinish() {
+                timeleft_view.setText("done!");
+            }
+        }.start();
     }
 
+    public void startTimer(TextView timeleft_view) {
 
+        Integer time = 0;
+        Integer limit = 60*1000;
+        Integer timeleft = limit;
+
+        while (time < limit) {
+            try {
+                wait(1000);
+            } catch(Exception e) {}
+            timeleft -= 1000;
+            timeleft_view.setText(timeleft + "s left");
+            time += 1000;
+        }
+    }
 }
