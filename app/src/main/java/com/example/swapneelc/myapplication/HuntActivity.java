a80dcd6f877717ac4babf7d3da92935524d1a8b2
flam316 <adam@rubiverse.net>
2016-04-03 01:13:17 -0400

    made some list stuff

--- app/src/main/java/com/example/swapneelc/myapplication/HuntActivity.java
+++ app/src/main/java/com/example/swapneelc/myapplication/HuntActivity.java
@@ -1,11 +1,13 @@
 package com.example.swapneelc.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class HuntActivity extends AppCompatActivity {
 
@@ -14,7 +16,13 @@ public class HuntActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hunt);
 
+        Intent i = getIntent();
 
+        String teamname = i.getStringExtra("teamname");
+
+        TextView teamname_view = (TextView) findViewById(R.id.teamname);
+        teamname_view.setText(teamname);
     }
+
 
 }
